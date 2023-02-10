0d5d26b1b53d9c2d3e749899548ea8abc1910fbe
Admin <Admin@TEB123008>
2023-02-10 08:45:04 +0100

    Add project files.

---
+++ Uppgift 3.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Har du gått ut på gymnasiet ? j/n");
+        string j_n = Console.ReadLine();
+
+        Console.WriteLine("Hur gammal är du ?");
+        int ålder = Convert.ToInt32(Console.ReadLine());
+
+        if (j_n == "j" && ålder < 22 && ålder > 18)
+        {
+            Console.WriteLine("Vi vill gärna anställa dig.");
+        }
+        else
+        {
+            Console.WriteLine("Vi letar tyvär efter annan personal just nu.");
+        }
+    }
+}
